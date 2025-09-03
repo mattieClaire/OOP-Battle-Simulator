0e6083694a2edc1265a6360772c7cdbd8a4e3b29
mattieClaire <mattie.lemons@ascte.org>
2025-09-03 14:17:35 -0500

    Goblin Game Hero

--- hero.py
+++ hero.py
@@ -1,3 +1,5 @@
+import random
+
 class Hero:
     """
     This is our hero blueprint.
@@ -13,6 +15,10 @@ class Hero:
     """
     
     def __init__(self, name):
+        self.name = name
+        self.health = 300
+        self.attack_power = random.randint(45, 60)
+    
         #TODO Set the hero's name.
         #TODO Set the hero's health. You might give the hero more health than a goblin.
         #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
@@ -20,9 +26,15 @@ class Hero:
 
     def strike(self):
         # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
-    
+        return random.randint(15, self.attack_power)
+
+
     def receive_damage(self, damage):
         # TODO Implement take_damage
         # TODO We should prevent health from going into the NEGATIVE
-    
+        self.health -= damage
+        print(f"{self.name} takes {damage} damage. Health is now {self.health}.")
+
     #TODO define is_alive
+    def is_alive(self):
+        return self.health > 0
